2ed089059be4709299f64450ecba65e48bdc3673
Bobin <bobinjolly93@gmail.com>
2019-06-19 11:24:16 +0530

    df

---
+++ src/input.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import {connect } from 'react-redux';
+
+class Input extends Component {
+    render() {
+        const contents = this.props.success ? null :
+        (
+            <form className="form-inline">
+                <input 
+                data-test= "input-box" id="word guess" type="text"
+                placeholder="Enter Guess"
+                />
+                <button
+                data-test="submit-button"
+                type="submit"
+                >Submit</button>
+            </form>
+        );
+        return (
+            <div data-test="component-input">
+                {contents}
+            </div>
+        );
+    };
+};
+
+const mapStateToProps = ({success}) => {
+    return {success} ;
+};
+
+export default connect(mapStateToProps) (Input);
