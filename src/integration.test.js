c247b8f075fe50490afabb77f0b006304bd2f8b5
Bobin <bobinjolly93@gmail.com>
2019-06-19 15:27:46 +0530

    wetrw

---
+++ src/integration.test.js
@@ -0,0 +1,13 @@
+import {storeFactory} from  './test/testUtils';
+import {guessWord} from './actions';
+
+describe('guessed word action dispatcher', () => {
+    describe('no  guessed words', () => {
+        test('updates state correctly for unsuccessful guess', () => {});
+        test('updates state correctly for successful guess', () => {});
+    });
+    describe('some  guessed words', () => {
+        test('updates state correctly for unsuccessful guess', () => {});
+        test('updates state correctly for successful guess', () => {});
+    });
+})
